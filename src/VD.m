77097a139a77a2ffe2319e3df161998376374c9c
Tiankui Zhang <tiankuizhang@email.arizona.edu>
2017-11-27 17:17:31 -0700

    add tests for different reduced volume

--- src/VD.m
+++ src/VD.m
@@ -57,17 +57,129 @@
 
 Kappa = 411;
 
-% vesicle dynamics
-[x, y, z] = meshgrid(linspace(-250,250,64)); % simulation domain in nm
+RV = 70; % reduced volume
 
-a = 215;
-b = 215;
-c = 70;
-F = sqrt(x.^2/a^2 + y.^2/b^2 + z.^2/c^2) - 1;
+switch RV
+	case 90
+
+		a = 210;
+		b = 210;
+		c = 114;
+
+		xv = linspace(-250,250,64);
+		yv = xv;
+		zv = xv(abs(xv)<150);
+
+		[x, y, z] = meshgrid(xv, yv, zv); % simulation domain in nm
+
+
+		F = sqrt(x.^2/a^2 + y.^2/b^2 + z.^2/c^2) - 1;
+
+	case 80
+
+		a = 200;
+		b = 150;
+		c = 71;
+
+		xv = linspace(-250,250,64);
+		yv = xv(abs(xv)<200);
+		zv = xv(abs(xv)<100);
+
+		[x, y, z] = meshgrid(xv, yv, zv); % simulation domain in nm
+
+
+		F = sqrt(x.^2/a^2 + y.^2/b^2 + z.^2/c^2) - 1;
+
+	case 70
+
+		a = 215;
+		b = 215;
+		c = 70;
+
+		xv = linspace(-250,250,64);
+		yv = xv;
+		zv = xv(abs(xv)<100);
+
+		[x, y, z] = meshgrid(xv, yv, zv); % simulation domain in nm
+
+
+		F = sqrt(x.^2/a^2 + y.^2/b^2 + z.^2/c^2) - 1;
+
+	case 65
+
+		a = 215;
+		b = 215;
+		c = 62.3;
+
+		xv = linspace(-250,250,64);
+		yv = xv;
+		zv = xv(abs(xv)<100);
+
+		[x, y, z] = meshgrid(xv, yv, zv); % simulation domain in nm
+
+
+		F = sqrt(x.^2/a^2 + y.^2/b^2 + z.^2/c^2) - 1;
+
+	case 60
+
+		a = 215;
+		b = 215;
+		c = 55.5;
+
+		xv = linspace(-250,250,64);
+		yv = xv;
+		zv = xv(abs(xv)<100);
+
+		[x, y, z] = meshgrid(xv, yv, zv); % simulation domain in nm
+
+
+		F = sqrt(x.^2/a^2 + y.^2/b^2 + z.^2/c^2) - 1;
+
+	case 55
+
+		a = 215;
+		b = 215;
+		c = 49.3;
+
+		xv = linspace(-250,250,64);
+		yv = xv;
+		zv = xv(abs(xv)<100);
+
+		[x, y, z] = meshgrid(xv, yv, zv); % simulation domain in nm
+
+
+		F = sqrt(x.^2/a^2 + y.^2/b^2 + z.^2/c^2) - 1;
+
+	case 50
+
+		a = 215;
+		b = 215;
+		c = 43.6;
+
+		xv = linspace(-250,250,64);
+		yv = xv;
+		zv = xv(abs(xv)<100);
+
+		[x, y, z] = meshgrid(xv, yv, zv); % simulation domain in nm
+
+
+		F = sqrt(x.^2/a^2 + y.^2/b^2 + z.^2/c^2) - 1;
+
+	otherwise
+		disp('unknown RV!');
+end
+
+% vesicle dynamics
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 %F = sqrt(x.^2+y.^2+z.^2) - 215;
 map = SD.SDF3(x,y,z,F);
 map.reinitialization( map.F )
-map.plotSurface(0,1,'g')
+%map.plotSurface(0,1,'g')
+
+%keyboard
 
 % save grids
 GridX = map.GD3.X;
@@ -75,15 +187,25 @@ GridY = map.GD3.Y;
 GridZ = map.GD3.Z;
 save(fullfile(Result_Folder,'Grid.mat'),'GridX','GridY','GridZ');
 
-loops = 5000;
+loops = 1000;
 Skip = 20;
 SkipR = 1;
 Dt = 2 * map.GD3.Dx ^ 4 / Kappa; % it is more like Dt*zeta(the drag coefficient)
 
 Eng = map.SurfaceIntegral(map.SC.^2);
 
+Eng_it = [];
+vol_it = [];
+ara_it = [];
+
+%keyboard
+
+ves = figure('Name', 'Vesicle Shape');
+eng = figure('Name', 'energy/volume/area vs time');
+
 for ii = 1:loops-1
 
+	cur_eng = map.SurfaceIntegral(map.SC.^2);
 	cur_vol = map.VolumeIntegral(1);
 	cur_ara = map.SurfaceIntegral(1);
 
@@ -100,7 +222,8 @@ for ii = 1:loops-1
 	%DV = (cur_vol - map.En_Volume) / Dt;
 	s_mat = [DA; DV];
 	% coefficient matrix from lagrange multiplier
-	c11 = map.SurfaceIntegral(map.SC.^2);
+	%c11 = map.SurfaceIntegral(map.SC.^2);
+	c11 = cur_eng;
 	c12 = - map.SurfaceIntegral(map.SC);
 	c21 = - c12;
 	%c22 = - map.Suf_Area;
@@ -110,10 +233,6 @@ for ii = 1:loops-1
 	lag_mul = c_mat\s_mat; % lagrange multiplier
 	Tension = lag_mul(1);
 	Pressure = lag_mul(2);
-
-	disp(['bending energy ratio ', num2str(ii), ': ', num2str(c11/Eng)]);
-	disp(['area error ', num2str(ii), ': ', num2str(cur_ara/map.Suf_Area)]);
-	disp(['volume error ', num2str(ii), ': ', num2str(cur_vol/map.En_Volume)]);
 
 	%% construct the force density operator
 	% an operator appearing in surface laplacian operator and curvature operator
@@ -133,8 +252,8 @@ for ii = 1:loops-1
 	% surface laplacian operator
 	SL = LOP - map.GD3.SparseDiag(map.SC) * ND;
 	% normal velocity operator without drag coefficient and Pressure
-	%NV = Kappa * ( ( SL + map.GD3.SparseDiag(co) ) * CvL ) - Tension * CvL;
-	NV = Kappa * ( ( SL + map.GD3.SparseDiag(co) ) * CvL );
+	NV = Kappa * ( ( SL + map.GD3.SparseDiag(co) ) * CvL ) - Tension * CvL;
+	%NV = Kappa * ( ( SL + map.GD3.SparseDiag(co) ) * CvL );
 	%NV = Kappa * SL  * CvL;
 
 	% the advection term without pressure
@@ -143,14 +262,19 @@ for ii = 1:loops-1
 	C = map.GD3.Idt - A;
 
 	F_old = map.F;
-	%S = C * F_old(:) - Dt * Pressure * map.Fg_1(:);
-	S = C * F_old(:);
+	S = C * F_old(:) - Dt * Pressure * map.Fg_1(:);
+	%S = C * F_old(:);
 
 	[L,U]=ilu(B,struct('type','nofill','milu','row'));
-	%F_new = gmres(B, S, 50, 1e-12, 10, L, U);
-	F_new = bicgstab(B, S, 1e-12, 200, L, U);
+	%F_new = gmres(B, S, [], 1e-12, 300, L, U);
+	F_new = bicgstab(B, S, 1e-12, 300, L, U);
+	%[F_new, flag] = bicgstab(B, S, 1e-12, 300, L, U);
+	%if flag
+	%	keyboard
+	%end
 	map.F = reshape(F_new, map.GD3.Size);
 
+	figure(ves)
 	clf
 	map.plotSurface(0,1,'g')
 	time = num2str(ii*Dt);
@@ -159,6 +283,10 @@ for ii = 1:loops-1
 	drawnow
 
 	if (mod(ii,SkipR)==0)
+		cur_vol_BR = map.VolumeIntegral(1);
+		cur_ara_BR = map.SurfaceIntegral(1);
+		disp(['area error before reinitialization ', num2str(ii), ': ', num2str(cur_ara_BR/map.Suf_Area)]);
+		disp(['volume error before reinitialization ', num2str(ii), ': ', num2str(cur_vol_BR/map.En_Volume)]);
 		map.reinitialization( reshape(F_new, map.GD3.Size) );
 	end
 
@@ -166,9 +294,48 @@ for ii = 1:loops-1
 	%	mov(count) = getframe(gcf);
 		DistanceMap = map.F;
 		saveas(gcf, fullfile(Pic,[num2str(ii),'AA_BR','.png']))
+		%saveas(gcf, fullfile(Pic,[num2str(ii),'AA_BR','.fig']))
 		save(fullfile(Mat,['DFV',num2str(ii),'AA_BR','.mat']),'DistanceMap')
 	%	count = count + 1;
 	end
+
+	Eng_it = [Eng_it, cur_eng/Eng];
+	vol_it = [vol_it, cur_vol/map.En_Volume-1]; 
+	ara_it = [ara_it, cur_ara/map.Suf_Area-1];
+
+	figure(eng)
+	clf
+
+	yyaxis left
+	plot(1:ii, Eng_it)
+	ylabel('energy measusred in initial energy')
+	xlabel('iteration')
+
+	yyaxis right
+	plot(1:ii, vol_it)
+	hold on
+	plot(1:ii, ara_it)
+	hold off
+	ylabel('deviation from initial area and volume')
+
+	legend('energy','volume error','area error')
+
+	drawnow
+
+	if (mod(ii,Skip)==0)
+	%	mov(count) = getframe(gcf);
+		%DistanceMap = map.F;
+		saveas(gcf, fullfile(Pic,['eng','.png']))
+		%saveas(gcf, fullfile(Pic,[num2str(ii),'AA_BR','.fig']))
+		%save(fullfile(Mat,['DFV',num2str(ii),'AA_BR','.mat']),'DistanceMap')
+	%	count = count + 1;
+	end
+
+
+
+	disp(['bending energy ratio ', num2str(ii), ': ', num2str(c11/Eng)]);
+	disp(['area error ', num2str(ii), ': ', num2str(cur_ara/map.Suf_Area)]);
+	disp(['volume error ', num2str(ii), ': ', num2str(cur_vol/map.En_Volume)]);
 
 	%keyboard
 end
